48c4b813d6562373639f3c4677940fdd0afcf5e2
admin <admin@admins-MacBook-Pro.local>
2020-01-02 14:15:46 -0600

    initial DRY logic in place

--- js/app.js
+++ js/app.js
@@ -3,19 +3,20 @@ const game = {
 	days: 0, // equivalent to rounds
 	trips: 0,
 
-	grind: function () {//
-		// for every instance "trips" is less than 1 ++ until trips is equal to 10
-		while (this.trips < 10){
-			const input = prompt ("Accept trip? (yes/no)") 
-			if (input === "yes") {
-				this.trips += 1
-				console.log(this.trips);
-				if (this.trips === 10){
-					this.days += 1
-					console.log(this.days);
-				}
-			}
-		} 
+	grind: function () { //stage method
+		
+		for (let i = 0; i < 11; i++) {
+		const input = prompt ("Accept trip? (yes/no)") 
+		if (input === "yes") {
+			this.trips += 1
+			
+			console.log(this.trips); 
+			if (this.trips % 10 === 0) {
+				this.days +=1
+				console.log(this.days); 
+				} 
+			}	
+		}
 	},//End of 1st Method
 
 }//End of Game Object 
--- pseudo/pseudo.js
+++ pseudo/pseudo.js
@@ -1,23 +1,31 @@
 pseudo.js
-const game = { 
-	points: 0,
-	days: 0, // equivalent to rounds
-	trips: 0,
 
-	grind: function () {//
-		// for every instance days is less than 1 ++ until trips is equal to 10
-		if (this.trips <= 10){
-			const = prompt ("Accept trip?" "(yes/no)" ) 
-			if (input === "yes") {
-				this.trips += 1
-			}
+// grind: function () { //stage method
+// 		// for every instance "trips" is less than 1 ++ until trips is equal to 10
+// 		while (this.trips < 10){
+			// const input = prompt ("Accept trip? (yes/no)") 
+			// if (input === "yes") {
+			// 	this.trips += 1
+// 				console.log(this.trips);
+// 				if (this.trips === 10){
+// 					this.days += 1
+// 					console.log(this.days);
+// 				}
+// 			}
+// 		} 
+// 	}//End of 1st Method
+// try drying the above with a for loop. 
+
+
+
+for (let i = 0; i < 50; i++) {
+	const input = prompt ("Accept trip? (yes/no)") 
+	if (input === "yes") {
+		this.trips += 1
+		if (this.trips 10 % === 0) {
+			this.days +=1 
 		} 
-		if (this.trips === 10){
-			this.day + 1
-		}
-
-	},
-
-}//End of Game Object 
+	}	
+}
 
 // call game object
