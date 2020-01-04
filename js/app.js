9a23172bb259caa07ad3a41899e8cd6aa7c99958
admin <admin@admins-MacBook-Pro.local>
2020-01-03 18:59:13 -0600

    goal message displays and updates; next make this happen dynamically

--- js/app.js
+++ js/app.js
@@ -1,23 +1,87 @@
-const game = { 
-	points: 0,
-	days: 0, // equivalent to rounds
-	trips: 0,
+//consider a Region class for which varying methods will be applied.
+// class Region {
+// 	constructor(neighborhood, downtown, airport, suburb) {
+// 		this.Neighborhood = northNeighborhood //or assign 
 
-	grind: function () { //stage method
-		
-		for (let i = 0; i < 11; i++) {
-		const input = prompt ("Accept trip? (yes/no)") 
-		if (input === "yes") {
-			this.trips += 1
-			
-			console.log(this.trips); 
-			if (this.trips % 10 === 0) {
-				this.days +=1
-				console.log(this.days); 
-				} 
-			}	
-		}
-	},//End of 1st Method
+// 	}
+// }
 
-}//End of Game Object 
-game.grind()
+// // class Trip
+// class Trip {
+// 	// constructor(){} takes origin -- 
+// 	constructor(origin, destination) {
+
+// 	const randomShort = [this.points = this.trips * .65, this.points = this.trips * .85, this.points = this.trips * .9]
+// 	const long = [this.points = this.trips * 1.2] 
+// 	// const random tip [] <-- 
+
+// 	this.origin: [
+// 	{neighborhood:function(){this.points = this.trips * .89}}, //put curly values inside of an array √√√
+// 	{downtown:function(){this.points = this.trips * .89}}, //try Math.floor(Math.random() * )
+// 	{airport:function(){this.points = this.trips * .89}},
+// 	{suburbub:function(){this.points = this.trips * .89}}
+// 	]
+// 	// based on origin, set random dest -- n, d, a, sub
+
+// 	if (origin === this.neighborhood){
+// 		//const destination = [Math.floor(Math.random() * origin.length)
+// 		//keep track of user 
+// 		this.points = this.trips * .89
+// 	// and short/long (dist) -- dictates earnings
+// 		}
+// 	// getValue() {}
+// 	}
+
+// } //end of class
+
+
+
+// vanilla equiv.
+
+
+const bills = [9, 14, 19]
+
+const goals = document.querySelector("#display-text")
+console.log(goals);
+
+const interface = document.createElement('p')
+console.log("is a p tag", interface);
+
+interface.innerText = `Make at least $${bills[Math.floor(Math.random() * bills.length)]} today` //if point is less than this number
+
+goals.appendChild(interface)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pseudo/pseudo.js
+++ pseudo/pseudo.js
@@ -1,31 +1,11 @@
 pseudo.js
 
-// grind: function () { //stage method
-// 		// for every instance "trips" is less than 1 ++ until trips is equal to 10
-// 		while (this.trips < 10){
-			// const input = prompt ("Accept trip? (yes/no)") 
-			// if (input === "yes") {
-			// 	this.trips += 1
-// 				console.log(this.trips);
-// 				if (this.trips === 10){
-// 					this.days += 1
-// 					console.log(this.days);
-// 				}
-// 			}
-// 		} 
-// 	}//End of 1st Method
-// try drying the above with a for loop. 
 
 
+// 1 
+	// win/lose scenario <-- if point = __ after day 1, you won the day! otherwise, you loose. 
+// 2
+	// make long trips and short trips metods that can be ran on Submarket classes 
 
-for (let i = 0; i < 50; i++) {
-	const input = prompt ("Accept trip? (yes/no)") 
-	if (input === "yes") {
-		this.trips += 1
-		if (this.trips 10 % === 0) {
-			this.days +=1 
-		} 
-	}	
-}
 
 // call game object
